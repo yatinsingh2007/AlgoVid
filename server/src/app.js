774dc3f8adc5e38f8497522d10933b6c2b368df3
yatinsingh2007 <yatin4591@gmail.com>
2025-11-11 11:00:51 +0530

    init

---
+++ server/src/app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+
+const { prisma } = require('../prisma/prismaConfig');
+
+const cors = require('cors');
+const app = express();
+app.use(cors({
+    origin: `${process.env.CLIENT_URL}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
+    app.listen(process.env.PORT , () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+  }
+}
+
+
+main()
+.catch((e) => {
+    console.log("Error in Connecting to Database:", e);
+}).finally(async () => {
+    await prisma.$disconnect();
+});
+
+module.exports = { app };
